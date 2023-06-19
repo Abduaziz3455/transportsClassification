707bf371c399fce9cb0212b04e3aa507a8046e05
Abduaziz3455 <abduaziz3455@gmail.com>
2023-06-19 15:13:25 +0500

    initial

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+# title
+st.title("Transportni klassifikatsiya qiluvchi model")
+
+# rasmni joylash
+file = st.file_uploader('Rasmni yuklash', type=['jpeg', 'png', 'gif', 'svg'])
+if file:
+    img = PILImage.create(file)
+    # model
+    model = load_learner('transport_model.pkl')
+
+    # prediction
+
+    pred, pred_id, probs = model.predict(img)
+    st.image(img)
+    st.success(f'Bashorat: {pred}')
+    st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}%')
+
+    # plotting
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
